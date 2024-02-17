5c397dd3271bd1a21fa7cb1758fb69ae51b2e5e9
sathish <mailsathishbala@gmail.com>
2024-02-17 15:11:25 +0530

    controller issue fixed

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -8,8 +8,8 @@ import '../../widgets/order_card.dart';
 import '../add_address/add_address_screen.dart';
 
 class CartScreen extends StatelessWidget {
-   const CartScreen({super.key});
-  
+    CartScreen({super.key});
+   final MyCartController controller = Get.put(MyCartController());
   @override
   Widget build(BuildContext context) {
     return  GetBuilder<MyCartController>(
