843fbdfc4b8c227ff4ad8955aa015bc6e5d20094
Sarthak <sarthak.cmk.al@gmail.com>
2024-02-22 23:59:16 +0530

    change

--- lib/screens/home_screen/components/popular_deal/popular_deals.dart
+++ lib/screens/home_screen/components/popular_deal/popular_deals.dart
@@ -1,29 +1,54 @@
+import 'dart:developer';
+
 import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:sutra_ecommerce/controllers/popular_deals.dart';
 import 'package:sutra_ecommerce/utils/screen_utils.dart';
 import 'package:sutra_ecommerce/widgets/product_card/product_card.dart';
 
 class PopularDealTab extends StatelessWidget {
-  const PopularDealTab({super.key});
+  final PopularDealController controller = Get.put(PopularDealController());
+  PopularDealTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SliverGrid(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-        crossAxisCount: 2,
-        childAspectRatio: 0.8,
-        crossAxisSpacing: getProportionateScreenWidth(8),
-      ),
-      delegate: SliverChildListDelegate(
-        [
-          const ProductCard(
-            isLeft: true,
+    return Obx(
+      () {
+        final popularDeals = controller.popularDeals;
+        return SliverGrid(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: (Get.width / Get.height) * 1.75,
+            crossAxisSpacing: getProportionateScreenWidth(8),
+            mainAxisSpacing: getProportionateScreenHeight(5),
           ),
-          const ProductCard(
-            isLeft: false,
+          delegate: SliverChildListDelegate(
+            List.generate(
+              controller.popularDeals.length,
+              (index) {
+                
+                log("popularDeals ${popularDeals.toString()}");
+                if (index % 2 != 0) {
+                  return ProductCard(
+                    isLeft: false,
+                    product: popularDeals[index],
+                  );
+                } else if (index == 0) {
+                  return ProductCard(
+                    isLeft: true,
+                    //addHandler: addCallback,
+                    product: popularDeals[index],
+                  );
+                }
+                return ProductCard(
+                  isLeft: true,
+                  product: popularDeals[index],
+                );
+              },
+            ),
           ),
-         
-        ],
-      ),
+        );
+      },
     );
   }
 }
