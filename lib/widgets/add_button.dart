7de03d97d3007db55659fd9846eb63408beffd92
Sarthak <sarthak.cmk.al@gmail.com>
2024-02-29 18:48:42 +0530

    add botton fix

--- lib/widgets/add_button.dart
+++ lib/widgets/add_button.dart
@@ -11,6 +11,18 @@ class AddButton extends StatefulWidget {
 
 class _AddButtonState extends State<AddButton> {
   int quantity = 0;
+  late FocusNode focusNode;
+  @override
+  void initState() {
+    super.initState();
+    focusNode = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    focusNode.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,77 +48,87 @@ class _AddButtonState extends State<AddButton> {
             ),
           )
         : Container(
-  decoration: BoxDecoration(
-    borderRadius: BorderRadius.circular(5), // Set the border radius here
-    border: Border.all(
-      color: kPrimaryBlue, // Specify the border color here
-    ),
-  ),
-  width: 100,
-  child: Row(
-    children: [
-      InkWell(
-        onTap: () {
-          setState(() {
-            if (quantity > 0) {
-              quantity--;
-            }
-          });
-        },
-        child: Container(
-          width: 30,
-          height: 30,
-          decoration: BoxDecoration(
-            //shape: BoxShape.circle,
-            color: kPrimaryBlue,
-          ),
-          child: const Icon(Icons.remove, size: 20, color: Colors.white),
-        ),
-      ),
-      Spacer(),
-      Container(
-  width: 30,
-  height: 30,
-  decoration: BoxDecoration(
-    //shape: BoxShape.circle,
-    //border: Border.all(color: kPrimaryBlue), // Circular border color
-  ),
-  child: TextField(
-    //readOnly: true, // Make the TextField read-only
-     inputFormatters: [FilteringTextInputFormatter.digitsOnly], // Allow only numeric input
-        keyboardType: TextInputType.number, // Set keyboard type to number
-    decoration: InputDecoration(
-      isDense: true,
-      border: InputBorder.none, // Remove default border
-      contentPadding: EdgeInsets.zero, // Remove default padding
-    ),
-    textAlign: TextAlign.center,
-    style: const TextStyle(color: kPrimaryBlue, fontSize: 16),
-    controller: TextEditingController(text: quantity.toString()),
-  ),
-),
+            decoration: BoxDecoration(
+              borderRadius:
+                  BorderRadius.circular(5), // Set the border radius here
+              border: Border.all(
+                color: kPrimaryBlue, // Specify the border color here
+              ),
+            ),
+            width: quantity.toString().length ==1?80:60 + 12.0 * quantity.toString().length,
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      if (quantity > 0) {
+                        quantity--;
+                      }
+                    });
+                  },
+                  child: Container(
+                    width: 25,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      //shape: BoxShape.circle,
+                      color: kPrimaryBlue,
+                    ),
+                    child:
+                        const Icon(Icons.remove, size: 20, color: Colors.white),
+                  ),
+                ),
+                const Spacer(),
+                Container(
+                  width: 12.0 * quantity.toString().length,
+                  height: 30,
+                  decoration: const BoxDecoration(
+                      //shape: BoxShape.circle,
+                      //border: Border.all(color: kPrimaryBlue), // Circular border color
+                      ),
+                  child: TextField(
+                    //readOnly: true, // Make the TextField read-only
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly
+                    ], // Allow only numeric input
+                    keyboardType:
+                        TextInputType.number, // Set keyboard type to number
+                    decoration: const InputDecoration(
+                      isDense: true,
 
-Spacer(),
-
-      InkWell(
-        onTap: () {
-          setState(() {
-            quantity++;
-          });
-        },
-        child: Container(
-          width: 30,
-          height: 30,
-          decoration: BoxDecoration(
-            //shape: BoxShape.circle,
-            color: kPrimaryBlue,
-          ),
-          child: const Icon(Icons.add, size: 20, color: Colors.white),
-        ),
-      ),
-    ],
-  ),
-);
-
+                      border: InputBorder.none, // Remove default border
+                      contentPadding:
+                          EdgeInsets.only(top: 5), // Remove default padding
+                    ),
+                    onChanged: (value) {
+                      
+                        quantity = int.parse(value);
+                      
+                    },
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(color: kPrimaryBlue, fontSize: 16),
+                    controller:
+                        TextEditingController(text: quantity.toString()),
+                  ),
+                ),
+                const Spacer(),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      quantity++;
+                    });
+                  },
+                  child: Container(
+                    width: 25,
+                    height: 30,
+                    decoration: const BoxDecoration(
+                      //shape: BoxShape.circle,
+                      color: kPrimaryBlue,
+                    ),
+                    child: const Icon(Icons.add, size: 20, color: Colors.white),
+                  ),
+                ),
+              ],
+            ),
+          );
   }
 }
