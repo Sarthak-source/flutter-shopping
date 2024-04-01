a88ca39f63b47e39b1a7d959a38a6abb3d37da66
Sarthak <sarthak.cmk.al@gmail.com>
2024-04-01 10:25:15 +0530

    CRASHLYTICS

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,6 +8,8 @@
 
 import 'package:connectivity_plus/src/connectivity_plus_web.dart';
 import 'package:device_info_plus/src/device_info_plus_web.dart';
+import 'package:firebase_analytics_web/firebase_analytics_web.dart';
+import 'package:firebase_core_web/firebase_core_web.dart';
 import 'package:flutter_pay_upi/flutter_pay_upi_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
 import 'package:geolocator_web/geolocator_web.dart';
@@ -20,6 +22,8 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   ConnectivityPlusWebPlugin.registerWith(registrar);
   DeviceInfoPlusWebPlugin.registerWith(registrar);
+  FirebaseAnalyticsWeb.registerWith(registrar);
+  FirebaseCoreWeb.registerWith(registrar);
   FlutterPayUpiWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
   GeolocatorPlugin.registerWith(registrar);
